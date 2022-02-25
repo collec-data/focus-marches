d090d81bee937dd44a6250de4af718b13a5cba8b
guenneuguesy <yann.guenneugues@sib.fr>
2022-02-25 11:31:32 +0100

    comment matomo

--- ui/inc/footer.php
+++ ui/inc/footer.php
@@ -18,36 +18,36 @@
   </div>
 </div>
   </footer>
-  <!-- Matomo -->
-  <script type="text/javascript">
-    var _paq = window._paq = window._paq || [];
-    _paq.push([function()
-      {
-        var self = this;
-        function getOriginalVisitorCookieTimeout()
-        {
-               var now = new Date(),
-               nowTs = Math.round(now.getTime() / 1000),
-               visitorInfo = self.getVisitorInfo();
-               var createTs = parseInt(visitorInfo[2]);
-               var cookieTimeout = 33696000; // 13 mois en secondes
-               var originalTimeout = createTs + cookieTimeout - nowTs;
-               return originalTimeout;
-         }
-         this.setVisitorCookieTimeout( getOriginalVisitorCookieTimeout() );
-       }
-      ]
-    );
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-      var u="//suivi-trafic.ternum-bfc.fr/";
-      _paq.push(['setTrackerUrl', u+'matomo.php']);
-      _paq.push(['setSiteId', '10']);
-      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-      g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-    })();
-  </script>
-  <!-- End Matomo Code -->
-</body>
+<!--  <!-- Matomo -->-->
+<!--  <script type="text/javascript">-->
+<!--    var _paq = window._paq = window._paq || [];-->
+<!--    _paq.push([function()-->
+<!--      {-->
+<!--        var self = this;-->
+<!--        function getOriginalVisitorCookieTimeout()-->
+<!--        {-->
+<!--               var now = new Date(),-->
+<!--               nowTs = Math.round(now.getTime() / 1000),-->
+<!--               visitorInfo = self.getVisitorInfo();-->
+<!--               var createTs = parseInt(visitorInfo[2]);-->
+<!--               var cookieTimeout = 33696000; // 13 mois en secondes-->
+<!--               var originalTimeout = createTs + cookieTimeout - nowTs;-->
+<!--               return originalTimeout;-->
+<!--         }-->
+<!--         this.setVisitorCookieTimeout( getOriginalVisitorCookieTimeout() );-->
+<!--       }-->
+<!--      ]-->
+<!--    );-->
+<!--    _paq.push(['trackPageView']);-->
+<!--    _paq.push(['enableLinkTracking']);-->
+<!--    (function() {-->
+<!--      var u="//suivi-trafic.ternum-bfc.fr/";-->
+<!--      _paq.push(['setTrackerUrl', u+'matomo.php']);-->
+<!--      _paq.push(['setSiteId', '10']);-->
+<!--      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];-->
+<!--      g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);-->
+<!--    })();-->
+<!--  </script>-->
+<!--  <!-- End Matomo Code -->-->
+<!--</body>-->
 </html>
