31d2ec3db34b959bd9b918df67cc9e91f6f2b920
guenneuguesy <yann.guenneugues@sib.fr>
2022-03-24 16:57:06 +0100

    gestion info greffe à partir de data gouv

--- batch/scripts/settings/settings.py
+++ batch/scripts/settings/settings.py
@@ -8,6 +8,7 @@ import logging
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@database:3306/marches_publics?charset=utf8'
 
 #URL
+URL_FICHIER_INFOS_GREFFE='https://www.data.gouv.fr/fr/datasets/r/a7790997-0a6a-48fa-b86b-1ef15f2a7552'
 URL_INFO_GREFFE='https://opendata.datainfogreffe.fr/api/records/1.0/search/?dataset=chiffres-cles-2020&q='
 TEMPO_CALL_INFO_GREFFE=5
 URL_API_SIREN_PERSO='http://152.228.212.208:3000/v3'
