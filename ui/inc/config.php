eeb51238e761d1cc3ca0d1b35cb1a41343e11529
guenneuguesy <yann.guenneugues@sib.fr>
2022-05-18 15:51:33 +0200

    modification pour Mégalis

--- ui/inc/config.php
+++ ui/inc/config.php
@@ -7,10 +7,10 @@
 
 //TODO automatiser ça
 // Nombre de mois glissants dont les requêtes doivent tenir compte
-$nb_mois = 38;
+$nb_mois = 41;
 
 // messages sur la dimension temporelle des des données
-$donnees_mises_a_jour = "25 février 2022";
+$donnees_mises_a_jour = "18 Mai 2022";
 $donnees_a_partir_du = "01 janvier 2019";
 
 ?>
