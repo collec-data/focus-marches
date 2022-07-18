1905f78cfa7a032a3a8ac6ef4abb8d5f50463d8b
guenneuguesy <yann.guenneugues@sib.fr>
2022-07-18 12:07:22 +0200

    mise en place getText

--- ui/localization.php
+++ ui/localization.php
@@ -1,9 +1,11 @@
 <?php
 // Set locale
+putenv("LANGUAGE=fr_FR.UTF-8");
 putenv("LC_ALL=fr_FR.UTF-8");
-putenv("LANGUAGE=");
 putenv("LANG=fr_FR.UTF-8");
 $res = setlocale(LC_ALL, 'fr_FR.UTF-8', 'fr_FR.', 'fr');
+//$res = setlocale(LANGUAGE, 'fr_FR.UTF-8', 'fr_FR.', 'fr');
+//$res = setlocale(LANG, 'fr_FR.UTF-8', 'fr_FR.', 'fr');
 
 //set domain
 bindtextdomain("megalis", "./locale");
