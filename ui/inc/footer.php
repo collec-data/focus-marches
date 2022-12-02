4fda0f0283fdb610785d460b3fd73027a254da6c
guenneuguesy <yann.guenneugues@sib.fr>
2022-12-02 16:31:45 +0100

    ajout logo financeurs

--- ui/inc/footer.php
+++ ui/inc/footer.php
@@ -1,56 +1,38 @@
-
-  <footer>
-    <div id="footerWrap">
-    <div id="footer" class="content has-text-centered">
-      <h3>A propos de cet outil</h3>
-      <div class="columns has-text-justified">
-        <div class="column">
-          <p>Cet outil est alimenté avec les données du profil d'acheteur de la salle de marchés de Mégalis Bretagne.</p>
-
-          <p>Il a été conçu par le service <b><a href="https://www.ternum-bfc.fr">Données Ouvertes</a></b> de <a href="https://www.ternum-bfc.fr/">ARNia</a> pour faciliter l'exploitation des informations contenues dans les données essentielles de la commande publique.</p>
+<div id="financeurs" class="container has-background-light">
+    <h2 class="is-size-3 center">Financement</h2>
+    <p class="center">Cet outil a bénéficié d'une évolution grâce à l'appui de :</p>
+    <div class="columns">
+        <div class="column center">
+            <img src="img/financeurs/logo_anct_marianne_100x325.png">
         </div>
-        <div class="column">
-          <p>Données publiées sous la <a href="pdf/ETALAB-Licence-Ouverte-v2.0.pdf">Licence Ouverte Version 2.0 - Etalab</a>. Le <a href="https://www.data.gouv.fr/fr/datasets/donnees-essentielles-du-profil-acheteur-megalis-bretagne/">téléchargement des données</a> se fait librement sur le portail opendata national.</p>
-          <p>Notre page <a href="mentions-legales.php">Mentions légales</a> contient des informations concernant la protection de la vie privée et vous permet de configurer le suivi ou pas par notre outil d'analyse de visites.  </p>
+        <div class="column center is-one-fifth">
+            <img src="img/financeurs/franceeelance_100x100.png">
         </div>
-      </div>
-        <p><?php echo gettext('Crédit de la photo du Pavillon nord des Halles Martenot, Rennes : <a href="https://commons.wikimedia.org/wiki/User:EdouardHue"> EdouardHue<a/>')?> </p>
-        <p><a href="https://www.recia.fr/"><img id="footerLogo" src="img/footer_logo.png" alt=<?php echo gettext('Territoires Numériques')?> /></a></p>
-  </div>
+        <div class="column center">
+            <img src="img/financeurs/nextgen_80x343.png">
+        </div>
+    </div>
+    <footer>
+        <div id="footerWrap">
+            <div id="footer" class="content has-text-centered">
+                <h3>A propos de cet outil</h3>
+                <div class="columns has-text-justified">
+                    <div class="column">
+                        <p>Cet outil est alimenté avec les données du profil d'acheteur de la salle de marchés de Mégalis Bretagne.</p>
+
+                        <p>Il a été conçu par le service <b><a href="https://www.ternum-bfc.fr">Données Ouvertes</a></b> de <a href="https://www.ternum-bfc.fr/">ARNia</a> pour faciliter l'exploitation des informations contenues dans les données essentielles de la commande publique.</p>
+                    </div>
+                    <div class="column">
+                        <p>Données publiées sous la <a href="pdf/ETALAB-Licence-Ouverte-v2.0.pdf">Licence Ouverte Version 2.0 - Etalab</a>. Le <a href="https://www.data.gouv.fr/fr/datasets/donnees-essentielles-du-profil-acheteur-megalis-bretagne/">téléchargement des données</a> se fait librement sur le portail opendata national.</p>
+                        <p>Notre page <a href="mentions-legales.php">Mentions légales</a> contient des informations concernant la protection de la vie privée et vous permet de configurer le suivi ou pas par notre outil d'analyse de visites.  </p>
+                    </div>
+                </div>
+                <p><?php echo gettext('Crédit de la photo du Pavillon nord des Halles Martenot, Rennes : <a href="https://commons.wikimedia.org/wiki/User:EdouardHue"> EdouardHue<a/>')?> </p>
+                <p><a href="https://www.recia.fr/"><img id="footerLogo" src="img/footer_logo.png" alt=<?php echo gettext('Territoires Numériques')?> /></a></p>
+            </div>
+        </div>
+    </footer>
+
+
 </div>
-  </footer>
-<!-- Matomo -->
-
-<!--  <script type="text/javascript">-->
-<!--    var _paq = window._paq = window._paq || [];-->
-<!--    _paq.push([function()-->
-<!--      {-->
-<!--        var self = this;-->
-<!--        function getOriginalVisitorCookieTimeout()-->
-<!--        {-->
-<!--               var now = new Date(),-->
-<!--               nowTs = Math.round(now.getTime() / 1000),-->
-<!--               visitorInfo = self.getVisitorInfo();-->
-<!--               var createTs = parseInt(visitorInfo[2]);-->
-<!--               var cookieTimeout = 33696000; // 13 mois en secondes-->
-<!--               var originalTimeout = createTs + cookieTimeout - nowTs;-->
-<!--               return originalTimeout;-->
-<!--         }-->
-<!--         this.setVisitorCookieTimeout( getOriginalVisitorCookieTimeout() );-->
-<!--       }-->
-<!--      ]-->
-<!--    );-->
-<!--    _paq.push(['trackPageView']);-->
-<!--    _paq.push(['enableLinkTracking']);-->
-<!--    (function() {-->
-<!--      var u="//suivi-trafic.ternum-bfc.fr/";-->
-<!--      _paq.push(['setTrackerUrl', u+'matomo.php']);-->
-<!--      _paq.push(['setSiteId', '10']);-->
-<!--      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];-->
-<!--      g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);-->
-<!--    })();-->
-<!--  </script>-->
-<!--</body>-->
-
-<!-- End Matomo Code -->
 </html>
