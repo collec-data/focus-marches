64568a627ce674c9f184747214bec02521ccedf1
monfortj <jordan.monfort.ext@sib.fr>
2024-02-29 11:45:04 +0100

    💄 ajout d'un fichier de style par défaut et spécifités de style dans style-override.css

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "démarrage de focus version : $ENVP"
-cp /app/personalisation/${ENVP}/style.css /app/css/
+cp /app/personalisation/${ENVP}/style-override.css /app/css/
 cp /app/personalisation/${ENVP}/*.png /app/img
 cp /app/personalisation/${ENVP}/favicon.ico /app
 cp /app/personalisation/${ENVP}/${ENVP}.po /app/locale/fr_FR/LC_MESSAGES/dico.po
