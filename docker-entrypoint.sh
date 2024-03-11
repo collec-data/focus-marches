89ca5650ff7e4e266f7f1284f46445e2106b72a4
monfortj <jordan.monfort.ext@sib.fr>
2024-03-11 10:33:12 +0100

    default focus marches style

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,12 @@ set -e
 echo "démarrage de focus version : $ENVP"
 if [[ -z "$ENVP" ]]; then
     echo "La variable de personnalisation ENVP n'est pas définie"
-    #enlèvement de ficher non nécessaires lorsque l'environnement n'est pas défini
-    rm -f css/style-override.css
-    rm -f img/focus-marches_logo_brand.png
+    cp /app/personalisation/default/style-override.css /app/css/
+    cp /app/personalisation/default/*.png /app/img
+    cp /app/personalisation/default/favicon.ico /app
+    # remove logo brands when using default env
+    rm -f /img/focus-marches_logo_brand.png
+    # no default messages with .po and .mo files
 else
     cp /app/personalisation/${ENVP}/style-override.css /app/css/
     cp /app/personalisation/${ENVP}/*.png /app/img
