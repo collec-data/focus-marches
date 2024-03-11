0f824bedd773ef8c4af5e06c2d3fa62c08956576
monfortj <jordan.monfort.ext@sib.fr>
2024-03-11 10:02:15 +0100

    navbar with opsn logo

--- ui/inc/nav.php
+++ ui/inc/nav.php
@@ -2,21 +2,20 @@
 </head>
 <body id="<?php echo $page; ?>" class="site ">
 
-  <div id="navWrap" style="display: flex;">
+  <div id="navWrap">
   <nav class="navbar is-light" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
       <a class="navbar-item" href="index.php">
         <img src="./img/focus-marches_logo.png" height="42">
       </a>
-      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarMenu">
+      <a id="menu-burger" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarMenu">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
       </a>
-    </div>
-
-
-    <div id="navbarMenu" class="navbar-menu">
+      <a id="brand-opsn" class="navbar-item" style="visibility: hidden;" href=<?php echo gettext('URL_OPSN')?> target="_blank">
+        <img src="./img/focus-marches_logo_brand.png" onload="this.parentElement.style.visibility='visible',this.parentElement.parentElement.style.visibility='visible'" height="42">
+      </a>
+      <div id="navbarMenu" class="navbar-menu">
       <div class="navbar-start">
         <a class="navbar-item" href="index.php"><i class="fas fa-home"></i>&nbsp;Accueil</a>
         <a class="navbar-item" href="acheteurs.php"><i class="fas fa-hotel"></i>&nbsp;Acheteurs</a>
@@ -24,13 +23,5 @@
         <!-- <a class="navbar-item" href="categories.php"><s>CPV</s></a> -->
         <a class="navbar-item" href="recherche.php"><i class="fas fa-search"></i>&nbsp;Recherche</a>
       </div>
-    </div>
-    
-    <div class="navbar-brand">
-      <a class="navbar-item" style="visibility: hidden;">
-        <img src="./img/focus-marches_logo_brand.png" onload="this.parentElement.style.visibility='visible'" height="42">
-      </a>
-    </div>
-
   </nav>
 </div><!-- ./navWrap -->
